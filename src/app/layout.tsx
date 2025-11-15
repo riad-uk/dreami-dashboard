534433f8e07fd008afc7aaef4f40bee2f8954385
Riad Kanane <one@kanane.com>
2025-11-15 18:59:13 +0000

    Minor Tweak

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body
-          className="antialiased"
-        >
+        <body className="antialiased">
           {children}
         </body>
       </html>
